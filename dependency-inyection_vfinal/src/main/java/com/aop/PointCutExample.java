8e028720ff4db37f9f96269ec1083205a036b67c
Circum-Baley <gonzalo.bahamondez@gmail.com>
2021-04-27 02:30:26 -0400

    Se Inicio HTTP - API REST - JSON

--- dependency-inyection_vfinal/src/main/java/com/aop/PointCutExample.java
+++ dependency-inyection_vfinal/src/main/java/com/aop/PointCutExample.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointCutExample {
 //	@Pointcut("within(TargetObject+)")
+//	Cuando utilizamos la anotacion de SpringAnnotation
 	@Pointcut("@annotation(SpringAnnotation)")
 	public void TargetObjectMethod() {
 		

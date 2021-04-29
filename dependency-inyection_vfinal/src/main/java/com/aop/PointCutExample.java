17c6f0d7f595e4bea6910cc7dd492067581d33a2
Circum-Baley <gonzalo.bahamondez@gmail.com>
2021-04-29 00:12:24 -0400

    La Clase PointCut cruza todos los metodos, dependendiendo de la prioriodad segun la regla de negocio
    This reverts commit 8e028720ff4db37f9f96269ec1083205a036b67c.

--- dependency-inyection_vfinal/src/main/java/com/aop/PointCutExample.java
+++ dependency-inyection_vfinal/src/main/java/com/aop/PointCutExample.java
@@ -4,7 +4,6 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointCutExample {
 //	@Pointcut("within(TargetObject+)")
-//	Cuando utilizamos la anotacion de SpringAnnotation
 	@Pointcut("@annotation(SpringAnnotation)")
 	public void TargetObjectMethod() {
 		
